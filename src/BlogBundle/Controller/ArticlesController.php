1feb669cf9d38e629188b8dde6b21ebda13f1475
Touchy69 <gear2and3@free.fr>
2017-05-30 18:45:36 +0200

    Blog bundle creation

--- src/BlogBundle/Controller/ArticlesController.php
+++ src/BlogBundle/Controller/ArticlesController.php
@@ -2,10 +2,36 @@
 
 namespace BlogBundle\Controller;
 
+use BlogBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticlesController extends Controller
 {
+
+    public function add2Action(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('content', TextType::class)
+            ->add('createdAt', TextType::class)
+            ->add('title', TextType::class)
+            ->add('envoyer', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            $url = $this->generateUrl('article_show', array('id' => $article->getId()));
+
+            return $this->redirect($url);
+        }
+        return $this->render('BlogBundle:Articles:add.html.twig', array('myForm' => $form->createView()));
+    }
+
     /**
      * GET & DISPLAY ALL ARTICLES
      * @return \Symfony\Component\HttpFoundation\Response
@@ -36,4 +62,25 @@ class ArticlesController extends Controller
         ]);
     }
 
+    /**
+     * @param $title
+     * @param $content
+     * @param $createdAt
+     * @param $author
+     * @param $themes
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function addAction($title, $content, $createdAt, $author, $themes)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = new Article();
+        //$article->setAuthor($author);
+        $article->setContent($content);
+        $article->setCreatedAt($createdAt);
+        $article->setTitle($title);
+        //$article->addTheme($themes);
+        $entityManager->persist($article);
+        $entityManager->flush();
+        return $this->redirectToRoute('article_show', array('id' => $article->getId()));
+    }
 }
