9b6d62c6184729fa20b46c7aedf977223a39fc2d
Touchy69 <gear2and3@free.fr>
2017-05-30 20:34:18 +0200

    Ajout de la création d'un article
    (validation à compléter)

--- src/BlogBundle/Controller/ArticlesController.php
+++ src/BlogBundle/Controller/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace BlogBundle\Controller;
 
 use BlogBundle\Entity\Article;
+use BlogBundle\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -10,27 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ArticlesController extends Controller
 {
-
-    public function add2Action(Request $request)
-    {
-        $article = new Article();
-        $form = $this->createFormBuilder($article)
-            ->add('content', TextType::class)
-            ->add('createdAt', TextType::class)
-            ->add('title', TextType::class)
-            ->add('envoyer', SubmitType::class)
-            ->getForm();
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($article);
-            $entityManager->flush();
-            $url = $this->generateUrl('article_show', array('id' => $article->getId()));
-
-            return $this->redirect($url);
-        }
-        return $this->render('BlogBundle:Articles:add.html.twig', array('myForm' => $form->createView()));
-    }
 
     /**
      * GET & DISPLAY ALL ARTICLES
@@ -63,14 +43,28 @@ class ArticlesController extends Controller
     }
 
     /**
-     * @param $title
-     * @param $content
-     * @param $createdAt
-     * @param $author
-     * @param $themes
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * Add an article
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function addAction($title, $content, $createdAt, $author, $themes)
+    public function addAction(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article,
+            array('action' => $this->generateUrl('article_add')));
+        $form->add('submit', SubmitType::class, array('label' => 'Add'));
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            $url = $this->generateUrl('article_show', array('id' => $article->getId()));
+            return $this->redirect($url);
+        }
+        return $this->render('BlogBundle:Articles:add.html.twig', array('myForm' => $form->createView()));
+    }
+
+    public function add2Action($title, $content, $createdAt, $author, $themes)
     {
         $entityManager = $this->getDoctrine()->getManager();
         $article = new Article();
@@ -82,5 +76,33 @@ class ArticlesController extends Controller
         $entityManager->persist($article);
         $entityManager->flush();
         return $this->redirectToRoute('article_show', array('id' => $article->getId()));
+    }
+
+    public function add3Action(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('content', TextType::class)
+            ->add('createdAt', TextType::class)
+            ->add('title', TextType::class)
+            ->add('envoyer', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            $url = $this->generateUrl('article_show', array('id' => $article->getId()));
+
+            return $this->redirect($url);
+        }
+        return $this->render('BlogBundle:Articles:add.html.twig', array('myForm' => $form->createView()));
+    }
+
+    public function navigationAction()
+    {
+        $repository = $this->getDoctrine()->getManager()->getRepository('BlogBundle:Article');
+        $article = $repository->findAll();
+        return $this->render('BlogBundle:Articles:navigation.html.twig', array('articles' => $article));
     }
 }
